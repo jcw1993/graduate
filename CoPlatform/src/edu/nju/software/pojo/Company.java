09822a1b9e053a611d496719fe844fc0fbdfe29b
jcw1993 <jcw19931007@gmail.com>
2015-04-01 15:45:15 +0800

    build project

---
+++ CoPlatform/src/edu/nju/software/pojo/Company.java
@@ -0,0 +1,40 @@
+package edu.nju.software.pojo;
+
+public class Company {
+	private int id;
+	private String name;
+	private String desc;
+	private String phone;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
